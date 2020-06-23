d8eda678f08459eb058c004f63f4ec738f24bead
Cindy Lay <cindylay524@gmail.com>
2020-06-24 00:50:44 +0900

    actual commit

--- classProjectile.py
+++ classProjectile.py
@@ -1,1 +1,7 @@
-"hi"
+# instead of animating something new or creating functionality for a projectile cell
+# just change the visual of the cell for each "frame" to reflect
+# a path that it is traveling.
+
+# for instance, maybe a cell could reflect a explosion for the 'projectile'
+# In the drawings the arrow and food cells aren't too far apart, so just displaying
+# an explosion graphic could show the projectile 'shooting' at the gopher
--- typeCell.py
+++ typeCell.py
@@ -3,7 +3,7 @@ import enum
 class CellType(enum.Enum):
     gopher = 0 #our agent!
     entrance = 1 #entry point for agent into the trap
-    wire = 2 #line that carries eletrical pulses from entrance
+    wire = 2 #line that carries electrical pulses from entrance
     arrow = 3 #where projectile is shot from
     dirt = 4 #where agent can roam
     food = 5 #empty space in environment
