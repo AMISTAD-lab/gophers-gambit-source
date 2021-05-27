3a740963ba236e39b01f7b2fafae242fff30f710
Amani Maina-Kilaas <amainakilaas@hmc.edu>
2021-05-27 10:26:35 -0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -12,9 +12,6 @@ def writeTojs(trapList):
             initTrapBoard: initial board for a trap.
             activeTrapCells: list of 2d lists with 1's in positions that are active, 0's otherwise.
             gopherTrapCells: List of tuples (each tuple is: [x, y, rotationType, state])
-        terrainList: a list of the form [initTerrainBoard, gopherTerrainCells]
-            initTerrainBoard: the initial board for the terrain (just a single 2d list!)
-            gopherTerrainCells: list of tuples (each tuple is: [x, y, rotationType]) (state is always alive)
     """  
     newFile = open(newjsFileName, "w") # create new file
     newFile.close()
